964430d6f4a52b4eb78313228f1402d482e8b034
victornpb <3372598+victornpb@users.noreply.github.com>
2018-09-30 12:41:51 -0300

    code organization, optimized memory and flash

--- ac-swing-servo.ino
+++ ac-swing-servo.ino
@@ -1,6 +1,8 @@
 #include <Arduino.h>
+
 #include <Servo.h>
 Servo myservo;
+const uint8_t SERVO_PIN = 9;
 
 #include <IRremote.h>
 const uint8_t RECV_PIN = 11;
@@ -8,37 +10,21 @@ IRrecv irrecv(RECV_PIN);
 decode_results results;
 
 
-const uint8_t SERVO_PIN = 9;
-
-enum ELGIN {
-  ELGIN_AUTO = 0x1FE6996,
-  ELGIN_ALTA = 0x1FE9966,
-  ELGIN_MEDIA = 0x1FED926,
-  ELGIN_BAIXA = 0x1FE59A6,
-  ELGIN_UP = 0x1FE29D6,
-  ELGIN_DOWN = 0x1FEA956,
-  ELGIN_FUNCAO = 0x1FEB946,
-  ELGIN_TIMER = 0x1FE7986,
-  ELGIN_DORMIR = 0x1FEF906,
-  ELGIN_POWER = 0x1FE39C6,
-};
-
-
 
 /** Led 13 blink on known command received */
 namespace Led {
 
-const uint8_t pin = 13;
-unsigned int duration = 50; //hold for x ms
+  const uint8_t pin = 13;
+  unsigned int duration = 50; //hold for x ms
 
-bool state;
-bool enabled;
-unsigned long ledBlinkTimeout;
+  bool state;
+  bool enabled;
+  unsigned long ledBlinkTimeout;
 
-bool flashing = false;
+  bool flashing = false;
 
-unsigned int flashingInterval = 50;
-unsigned long flashingTimeout;
+  unsigned int flashingInterval = 50;
+  unsigned long flashingTimeout;
 
   void blink(unsigned int d) {
     flashing = false;
@@ -47,18 +33,18 @@ unsigned long flashingTimeout;
     ledBlinkTimeout = millis();
     digitalWrite(pin, HIGH);
   }
-  
-  void on(){
+
+  void on() {
     state = true;
     digitalWrite(pin, HIGH);
   }
 
-  void off(){
+  void off() {
     state = false;
     digitalWrite(pin, LOW);
   }
 
-  void flash(unsigned int d, unsigned int i){
+  void flash(unsigned int d, unsigned int i) {
     enabled = true;
     duration = d;
     flashingInterval = i;
@@ -67,29 +53,21 @@ unsigned long flashingTimeout;
     ledBlinkTimeout = millis();
   }
 
-  void routine(unsigned long currentMillis)
-  {
-    if (enabled)
-    {
-      if (currentMillis - ledBlinkTimeout > duration)
-      {
+  void routine(unsigned long currentMillis) {
+    if (enabled) {
+      if (currentMillis - ledBlinkTimeout > duration) {
         enabled = false;
         flashing = false;
         digitalWrite(pin, LOW);
       }
 
-      if (flashing && ((currentMillis - flashingTimeout) > flashingInterval))
-      {
+      if (flashing && ((currentMillis - flashingTimeout) > flashingInterval)) {
         flashingTimeout = currentMillis;
         state = !state;
         digitalWrite(pin, state);
       }
     }
-
- 
-
-
-}
+  }
 
 }
 
@@ -97,158 +75,190 @@ unsigned long flashingTimeout;
 
 namespace ServoProgram {
 
-const uint8_t ANGLE_MIN = 90;
-const uint8_t ANGLE_MAX = 180;
+  const uint8_t ANGLE_MIN = 90;
+  const uint8_t ANGLE_MAX = 180;
 
-uint8_t angle = ANGLE_MIN; //current angle
-unsigned long startedMillis; //used for delays
+  uint8_t angle = ANGLE_MIN; //current angle
+  unsigned long startedMillis; //used for delays
 
-uint8_t stepSize;
-unsigned int stepDelay;
+  uint8_t stepSize;
+  unsigned int stepDelay;
 
-uint8_t parkStep;
+  uint8_t parkStep;
 
-uint8_t seekTargetAngle = ANGLE_MAX;
+  uint8_t seekTargetAngle = ANGLE_MAX;
 
-enum modes {
-  MODE_OFF,
-  MODE_IDLE,
-  MODE_PARK,
-  MODE_SEEK,
-  MODE_SWING,
-};
+  const unsigned int IDLE_TIMEOUT = 3000;
 
-uint8_t mode = MODE_IDLE;
+  enum modes {
+    MODE_OFF,
+    MODE_IDLE,
+    MODE_PARK,
+    MODE_SEEK,
+    MODE_SWING,
+  };
 
-void off() {
-  Serial.println("\nOFF");
+  uint8_t mode = MODE_IDLE;
 
-  mode = MODE_OFF;
-  myservo.detach();
-}
+  void off() {
+    Serial.print("\nOFF");
 
-void idle() {
-  Serial.println("\nIDLE");
-
-  mode = MODE_IDLE;
-  stepDelay = 3000;
-}
-
-void seek(uint8_t a) {
-  Serial.print("\nSEEK:");
-  Serial.print(a);
-
-  myservo.attach(SERVO_PIN);
-
-  //constrain
-  if (a < ANGLE_MIN) a = ANGLE_MIN;
-  else if (a > ANGLE_MAX) a = ANGLE_MAX;
-
-  mode = MODE_SEEK;
-  stepSize = 10;
-  stepDelay = 50;
-  seekTargetAngle = a;
-}
-
-void swing() {
-  Serial.print("\nSWING");
-  myservo.attach(SERVO_PIN);
-
-  stepSize = 1;
-  stepDelay = 111; //roughtly 10s from min to max
-  mode = MODE_SWING;
-}
-
-
-
-void park() {
-  Serial.print("\nPARK");
-  parkStep = 0;
-  stepDelay = 0;
-  mode = MODE_PARK;
-}
-
-void idleRoutine(unsigned long currentMillis) {
-
-  if (currentMillis - startedMillis > stepDelay) {
-    startedMillis = currentMillis;
-    off();
+    mode = MODE_OFF;
+    myservo.detach();
   }
-}
 
-void parkRoutine(unsigned long currentMillis) {
-  if (currentMillis - startedMillis > stepDelay) {
-    startedMillis = currentMillis;
-    uint8_t a = angle;
+  void idle() {
+    Serial.print("\nIDLE");
 
-    parkStep++;
-    if (parkStep == 0) {
-      stepDelay = 30;
-      a -= 8;
-    }
-    else if (parkStep == 1) {
-      stepDelay = 30;
-      a += 7;
-    }
-    else if (parkStep == 2) {
-      stepDelay = 30;
-      a -= 6;
-    }
-    else if (parkStep == 3) {
-      stepDelay = 30;
-      a += 5;
-    }
-    else if (parkStep == 4) {
-      stepDelay = 30;
-      a -= 4;
-    }
-    else if (parkStep == 5) {
-      stepDelay = 30;
-      a += 3;
-    }
-    else if (parkStep == 6) {
-      stepDelay = 30;
-      a -= 2;
-    }
-    else if (parkStep == 7) {
-      stepDelay = 30;
-      a += 1;
-    }
-    else if (parkStep == 8) {
-      stepDelay = 100;
-      a = angle;
-    }
-    else {
-      idle();
-      return;
-    }
+    mode = MODE_IDLE;
+    stepDelay = IDLE_TIMEOUT;
+  }
 
-    Serial.print(' ');
+  void seek(uint8_t a) {
+    Serial.print("\nSEEK:");
     Serial.print(a);
 
-    myservo.write(a);
+    myservo.attach(SERVO_PIN);
+
+    //constrain
+    if (a < ANGLE_MIN) a = ANGLE_MIN;
+    else if (a > ANGLE_MAX) a = ANGLE_MAX;
+
+    mode = MODE_SEEK;
+    stepSize = 10;
+    stepDelay = 50;
+    seekTargetAngle = a;
   }
-}
+
+  void swing() {
+    Serial.print("\nSWING");
+    myservo.attach(SERVO_PIN);
+
+    stepSize = 1;
+    stepDelay = 111; //roughtly 10s from min to max
+    mode = MODE_SWING;
+  }
+
+  void park() {
+    Serial.print("\nPARK");
+    parkStep = 0;
+    stepDelay = 0;
+    mode = MODE_PARK;
+  }
 
 
-void seekRoutine(unsigned long currentMillis) {
-  if (angle != seekTargetAngle) {
+
+  void idleRoutine(unsigned long currentMillis) {
+    if (currentMillis - startedMillis > stepDelay) { //after idling for this long
+      startedMillis = currentMillis;
+      off(); //turn off
+    }
+  }
+
+  void parkRoutine(unsigned long currentMillis) {
     if (currentMillis - startedMillis > stepDelay) {
+      startedMillis = currentMillis;
+      uint8_t a = angle;
 
-      if (angle < seekTargetAngle) {
-        angle += stepSize;
-        if (seekTargetAngle - angle < stepSize) angle = seekTargetAngle;
+      parkStep++;
+      if (parkStep == 0) {
+        stepDelay = 30;
+        a -= 8;
+      }
+      else if (parkStep == 1) {
+        stepDelay = 30;
+        a += 7;
+      }
+      else if (parkStep == 2) {
+        stepDelay = 30;
+        a -= 6;
+      }
+      else if (parkStep == 3) {
+        stepDelay = 30;
+        a += 5;
+      }
+      else if (parkStep == 4) {
+        stepDelay = 30;
+        a -= 4;
+      }
+      else if (parkStep == 5) {
+        stepDelay = 30;
+        a += 3;
+      }
+      else if (parkStep == 6) {
+        stepDelay = 30;
+        a -= 2;
+      }
+      else if (parkStep == 7) {
+        stepDelay = 30;
+        a += 1;
+      }
+      else if (parkStep == 8) {
+        stepDelay = 100;
+        a = angle;
       }
       else {
-        angle -= stepSize;
-        if (angle - seekTargetAngle < stepSize) angle = seekTargetAngle;
+        idle();
+        return;
       }
 
-      //constrain
-      if (angle <= ANGLE_MIN) seekTargetAngle = angle = ANGLE_MIN;
-      else if (angle >= ANGLE_MAX) seekTargetAngle = angle = ANGLE_MAX;
+      Serial.print(' ');
+      Serial.print(a);
 
+      myservo.write(a);
+    }
+  }
+
+
+  void seekRoutine(unsigned long currentMillis) {
+    if (angle != seekTargetAngle) {
+      if (currentMillis - startedMillis > stepDelay) {
+        startedMillis = currentMillis;
+
+        //integrate
+        if (angle < seekTargetAngle) {
+          angle += stepSize;
+          if (seekTargetAngle - angle < stepSize) angle = seekTargetAngle; //final step
+        }
+        else {
+          angle -= stepSize;
+          if (angle - seekTargetAngle < stepSize) angle = seekTargetAngle; //final step
+        }
+
+        //constrain
+        if (angle <= ANGLE_MIN) seekTargetAngle = angle = ANGLE_MIN;
+        else if (angle >= ANGLE_MAX) seekTargetAngle = angle = ANGLE_MAX;
+
+
+        Serial.print(' ');
+        Serial.print(angle);
+
+        myservo.write(angle);
+      }
+    }
+    else {
+      park();
+      // Led::blink(100);
+    }
+  }
+
+  void swingRoutine(unsigned long currentMillis) {
+
+    if (currentMillis - startedMillis > stepDelay) {
       startedMillis = currentMillis;
+
+      //integrate
+      if (angle < seekTargetAngle) angle += stepSize;
+      else angle -= stepSize;
+
+      //constrain
+      if (angle <= ANGLE_MIN) angle = ANGLE_MIN;
+      else if (angle >= ANGLE_MAX) angle = ANGLE_MAX;
+
+      //if reached a stable point, make it oscilate
+      if (angle == ANGLE_MIN) seekTargetAngle = ANGLE_MAX;
+      else if (angle == ANGLE_MAX) seekTargetAngle = ANGLE_MIN;
+      else if (angle == seekTargetAngle) seekTargetAngle = ANGLE_MAX;
 
       Serial.print(' ');
       Serial.print(angle);
@@ -256,79 +266,51 @@ void seekRoutine(unsigned long currentMillis) {
       myservo.write(angle);
     }
   }
-  else {
-    park();
-    // Led::blink(100);
-  }
-}
 
-void swingRoutine(unsigned long currentMillis) {
 
-  if (currentMillis - startedMillis > stepDelay) {
-    startedMillis = currentMillis;
-
-    //integrate
-    if (angle < seekTargetAngle) angle += stepSize;
-    else angle -= stepSize;
-
-    //constrain
-    if (angle <= ANGLE_MIN) {
-      angle = ANGLE_MIN;
+  void routine(unsigned long currentMillis) {
+    switch (mode) {
+      case MODE_OFF:
+        //do nothing
+        break;
+      case MODE_IDLE:
+        idleRoutine(currentMillis);
+        break;
+      case MODE_PARK:
+        parkRoutine(currentMillis);
+        break;
+      case MODE_SEEK:
+        seekRoutine(currentMillis);
+        break;
+      case MODE_SWING:
+        swingRoutine(currentMillis);
+        break;
     }
-    else if (angle >= ANGLE_MAX) {
-      angle = ANGLE_MAX;
-    }
-
-    //reached stable point?
-    if (angle == ANGLE_MIN) seekTargetAngle = ANGLE_MAX;
-    else if (angle == ANGLE_MAX) seekTargetAngle = ANGLE_MIN;
-    else if (angle == seekTargetAngle) seekTargetAngle = ANGLE_MAX;
-
-    Serial.print(' ');
-    Serial.print(angle);
-
-    myservo.write(angle);
   }
 }
 
 
-void routine(unsigned long currentMillis) {
-  switch (mode) {
-    case MODE_PARK:
-      parkRoutine(currentMillis);
-      break;
-    case MODE_SEEK:
-      seekRoutine(currentMillis);
-      break;
-    case MODE_SWING:
-      swingRoutine(currentMillis);
-      break;
-    case MODE_IDLE:
-      idleRoutine(currentMillis);
-      break;
 
+namespace Remote {
 
-  }
-}
+  enum ELGIN_REMOTE {
+    ELGIN_AUTO = 0x1FE6996,
+    ELGIN_ALTA = 0x1FE9966,
+    ELGIN_MEDIA = 0x1FED926,
+    ELGIN_BAIXA = 0x1FE59A6,
+    ELGIN_UP = 0x1FE29D6,
+    ELGIN_DOWN = 0x1FEA956,
+    ELGIN_FUNCAO = 0x1FEB946,
+    ELGIN_TIMER = 0x1FE7986,
+    ELGIN_DORMIR = 0x1FEF906,
+    ELGIN_POWER = 0x1FE39C6,
+  };
 
+  const unsigned int HOLD_TIMEOUT = 500; //time for a button to be considered as holding
+  const unsigned int REPEAT_THROTTLE = 300; //throttle for hold to repeat action
 
-}
+  void routine(unsigned long currentMillis) {
 
-
-enum Actions{
-  Action_NoAction,
-  Action_Swing,
-  Action_VoldownAction,
-  Action_MuteAction,
-};
-
-const int RepeatTimeout = 300; //ms
-
-byte processRemote(){
-    
-    byte action = Action_NoAction;
-    
-    
     static unsigned long previousMillis;
 
     static unsigned long lastCode;
@@ -336,115 +318,103 @@ byte processRemote(){
 
     static bool preventRepeat = false;
 
-    if(irrecv.decode(&results)) {
-        unsigned long currentMillis = millis();
+    if (irrecv.decode(&results)) {
+      unsigned long code = results.value;
 
-        unsigned long code = results.value;
-        if(code!=0xFFFFFFFF){
-          lastCode = code;
-          lastCodeMillis = currentMillis;
-          preventRepeat = false; //this is a new code
-        }
-        else { //repeat
-          // code = lastCode;
-        }
+      // Serial.print("\nCODE:");
+      // Serial.print(code, HEX);
 
-        // Serial.print(F("\nCODE:"));
-        // Serial.print(code, HEX);
-        
-        
-        switch(code){
-            
+      if (code != REPEAT) {
+        lastCode = code;
+        lastCodeMillis = currentMillis;
+        preventRepeat = false; //this is a new code
+      }
+      else { //repeat
+        // code = lastCode;
+      }
+
+      switch (code) {
+
+        case ELGIN_AUTO:
+          if (ServoProgram::mode == ServoProgram::MODE_SWING) { //stop swing
+            ServoProgram::park();
+            Led::flash(1500, 250); //-_-_-_
+          }
+          break;
+
+        case ELGIN_ALTA:
+          if (ServoProgram::mode != ServoProgram::MODE_SWING) { //seek if not swinging
+            if (ServoProgram::angle > ServoProgram::ANGLE_MIN) {
+              ServoProgram::seek(ServoProgram::angle - 5);
+              Led::blink(500);
+            }
+          }
+          break;
+
+        case ELGIN_BAIXA:
+          if (ServoProgram::mode != ServoProgram::MODE_SWING) { //seek if not swinging
+            if (ServoProgram::angle < ServoProgram::ANGLE_MAX) {
+              ServoProgram::seek(ServoProgram::angle + 5);
+              Led::blink(500);
+            }
+          }
+          break;
+
+        case REPEAT:
+
+          // if(currentMillis - previousMillis < REPEAT_THROTTLE){
+          //   switch (lastCode) { // "hold to repeat" actions
+          //     case ELGIN_AUTO:
+          //     previousMillis = currentMillis;
+          //     //do something
+          //     break;
+          //   }
+          // }
+  
+          unsigned int holdTime = currentMillis - lastCodeMillis;
+          if (preventRepeat == false && holdTime > HOLD_TIMEOUT) { //after holding for this long
+            switch (lastCode) { //"press and hold" actions
+
             case ELGIN_AUTO:
-                // action = Action_Swing;
-                // actionTimestamp = currentMillis;
-                 if(ServoProgram::mode==ServoProgram::MODE_SWING){
-                    ServoProgram::park();
-                    Led::flash(1500, 250); //-_-_-_
-                 }
+              preventRepeat = true; //only trigger once
+              ServoProgram::swing();
+              Led::blink(10000);
+              break;
 
-                break;
+            case ELGIN_MEDIA:
+              preventRepeat = true; //only trigger once
+              ServoProgram::seek(155); //center
+              Led::blink(3000);
+              break;
+            }
+          }
+          break;
+      }
 
-                case ELGIN_ALTA:
-                
-                if(ServoProgram::mode!=ServoProgram::MODE_SWING){
-
-                  if(ServoProgram::angle>ServoProgram::ANGLE_MIN){
-                    ServoProgram::seek(ServoProgram::angle-5);
-                    Led::blink(500);
-                  }
-                }
-                break;
-
-                case ELGIN_BAIXA:
-                 if(ServoProgram::mode!=ServoProgram::MODE_SWING){
-                  if(ServoProgram::angle<ServoProgram::ANGLE_MAX){
-                    ServoProgram::seek(ServoProgram::angle+5);
-                    Led::blink(500);
-                  }
-                 }
-                break;
-
-            case REPEAT:
-                // if(currentMillis - previousMillis < RepeatTimeout){
-                //     if(lastAction == Action_Swing || lastAction == Action_VoldownAction){ //only enable repeat for this actions
-                //         action = lastAction;
-                //     }
-                // }
-
-                unsigned int holdTime = currentMillis - lastCodeMillis;
-                if (holdTime > 600 && preventRepeat==false) { //after holding for this long
-
-
-                  switch(lastCode){
-                    case ELGIN_AUTO:
-                        preventRepeat = true; //only trigger once
-
-                        Serial.print("HOLD ");
-                        Serial.print(lastCode);
-                        Serial.println();
-
-
-
-                        // if(ServoProgram::mode==ServoProgram::MODE_SWING){
-                        //   ServoProgram::park();
-                        //   Led::flash(1000, 100);
-                        // }
-                        // else{
-                          ServoProgram::swing();
-                          Led::blink(10000);
-                        // }
-
-                      break;
-
-                      case ELGIN_MEDIA:
-                        preventRepeat = true; //only trigger once
-
-                        Serial.print("HOLD ");
-                        Serial.print(lastCode);
-                        Serial.println();
-
-                        ServoProgram::seek(155);
-
-                        // Led::flash(2000, 200);
-                        Led::blink(3000);
-
-                        // action = Action_Swing;
-                      break;
-                    }
-                    
-                }
-                break;
-        }
-
-        irrecv.resume();
-
- 
-        
-        // if(action != Action_NoAction) Led::blink(100);
+      irrecv.resume();
     }
+  }
+}
 
-    return action;
+
+
+namespace SerialProgram {
+
+  void routine(unsigned long currentMillis){
+    if (Serial.available() > 0) {
+      char c = Serial.read();
+      switch(c){
+        case 's': //send "s" to swing
+        ServoProgram::swing();
+        break;
+        case 'a': //send "a90" to seek to 90 degrees 
+          int a = Serial.parseInt();
+          ServoProgram::seek(a);
+          break;
+      }
+    }
+  }
+
 }
 
 
@@ -452,44 +422,21 @@ byte processRemote(){
 void setup() {
   // put your setup code here, to run once:
 
-  myservo.attach(SERVO_PIN);
   pinMode(13, OUTPUT);
 
-  //  myservo.write(180);
-  //  delay(1000);
-  //  myservo.write(90);
-  //  delay(1000);
+  myservo.attach(SERVO_PIN);
 
+  irrecv.enableIRIn();
+  
   Serial.begin(9600);
-  Serial.println("\nBOOT");
-
-    irrecv.enableIRIn();
+  Serial.print("\nBOOT\n");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (Serial.available() > 0) {
-    char c = Serial.read();
-
-    if (c == 's') {
-      ServoProgram::swing();
-    }
-    else if (c == 'a') {
-      int a = Serial.parseInt();
-      ServoProgram::seek(a);
-    }
-    else {
-      //      Serial.println("garbage");
-    }
-  }
-
-  byte action = processRemote();
-
   unsigned long m = millis();
 
+  SerialProgram::routine(m);
+  Remote::routine(m);
   ServoProgram::routine(m);
   Led::routine(m);
 }
-
-
-
